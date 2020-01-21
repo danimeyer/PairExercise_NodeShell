7eba7244f04c834e4d4b49b80eaafe2a71031803
Dani <dani.meyer.nd@gmail.com>
2020-01-21 15:00:37 -0500

    Dani added cat feature

--- bash.js
+++ bash.js
@@ -1,14 +1,20 @@
-
 const pwd = require('./pwd');
+const ls = require('./ls');
+const cat = require('./cat');
 
-process.stdout.write('prompt >');
+process.stdout.write('prompt > ');
 
-process.stdin.on('data', (data) => {
+process.stdin.on('data', data => {
   const cmd = data.toString().trim();
+  const cmd2 = cmd.split(' ');
 
-  if (cmd==='pwd') {
+  if (cmd === 'pwd') {
     pwd();
+  } else if (cmd === 'ls') {
+    ls();
+  } else if (cmd2[0] === 'cat') {
+    cat(cmd2[1]);
   } else {
     process.stdout.write('\nprompt > ');
   }
-})
+});
---
+++ cat.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+module.exports = function(fileName) {
+  fs.readFile(`./${fileName}`, 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(data);
+      process.stdout.write("prompt > ");
+    }
+  })
+}
