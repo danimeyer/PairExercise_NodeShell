13e6926bac6c1b43eb5dce1102f1bd8324252e4d
Lilly Oh <ohlilly@gmail.com>
2020-01-21 13:02:21 -0500

    bash.js and pwd.js

--- bash.js
+++ bash.js
@@ -1,8 +1,14 @@
+
+const pwd = require('./pwd');
+
 process.stdout.write('prompt >');
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
 
-  process.stdout.write('You typed: ' + cmd);
-  process.stdout.write('\nprompt > ');
+  if (cmd==='pwd') {
+    pwd();
+  } else {
+    process.stdout.write('\nprompt > ');
+  }
 })
